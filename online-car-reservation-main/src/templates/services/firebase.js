c2671fc5c0d026909c776a4ee47ad3cb63f3f4bb
Ankith <61228524+Ankith-01@users.noreply.github.com>
2022-07-05 17:26:29 +0530

    Add files via upload

---
+++ online-car-reservation-main/src/templates/services/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "your are apikey",
+    authDomain: "your are authDomain",
+    projectId: "your are projectId",
+    storageBucket: "your are storageBucket",
+    messagingSenderId: "your are messagingSenderId",
+    appId: "your are appId",
+    measurementId: "your are measurementId"
+  };
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore();
+const auth = firebase.auth();
+const store = firebase.database();
+// const functions = require('firebase-function');
+
+// exports.helloworld = functions.https.onRequest((request,response) =>{
+//   response.send("hello world!");
+// });
+
+
+export {db,auth,firebaseConfig,store};
